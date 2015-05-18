d13c1363c5dd779abf136e3ce87b5476cde6a4d2
Amanda Canyon <acanyon@gmail.com>
2015-05-18 11:48:16 -0700

    functioning carousel

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -16,7 +16,40 @@ $(function () {
 
 
     /** Carousel functions **/
-
+    var $hero_panels = $body.find('.hero_panels');
+    var CAROUSEL_TIMEOUT = 4000; // ms
+    var child_index = 0;
+    function set_carousel_timeout (time) {
+        window.setTimeout(function () {
+            $hero_panels.addClass('animate');
+            child_index = child_index + 1;
+            // var panel_width = $($hero_panels.children()[0]).width();
+            var new_left = -1 * $($hero_panels.children()[child_index]).position().left;
+            $hero_panels.css('left', new_left + 'px');
+            // should reset panels ?
+            if (child_index + 1 >= $hero_panels.children().length) {
+                window.setTimeout(function () {
+                    $hero_panels.removeClass('animate');
+                    var last_panel = $hero_panels.children().length - 1;
+                    var $last_panel = $hero_panels.children().last();
+                    _.each($hero_panels.children(), function (panel, i) {
+                        var $panel = $(panel);
+                        if (i !== last_panel) {
+                            $panel.remove();
+                            $last_panel.after($panel);
+                            $last_panel = $panel;
+                        }
+                    });
+                    $hero_panels.css('left', 0);
+                    child_index = 0;
+                    set_carousel_timeout(CAROUSEL_TIMEOUT/2);
+                }, CAROUSEL_TIMEOUT/2);
+            } else {
+                set_carousel_timeout();
+            }
+        }, time || CAROUSEL_TIMEOUT);
+    }
+    set_carousel_timeout();
 
     /** Email form functions **/
 
