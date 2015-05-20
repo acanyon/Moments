5b686c53c86cee97de707a45f6fb9c3db7265c65
Amanda Canyon <acanyon@gmail.com>
2015-05-20 16:25:10 -0700

    moved map_config to its own file

--- citycruzer-phonegap/www/js/app.js
+++ citycruzer-phonegap/www/js/app.js
@@ -1,17 +1,6 @@
 "use strict" ;
 
 var map;
-var MAP_CONF = {
-    'center': {
-        'lat': 37.7833, 'lng': -122.4167, // san francisco
-        'zoom': 12
-    },
-
-    'markers': [
-        { 'lat': 37.7966021, 'lng': -122.4233821 },
-        { 'lat': 37.7958912, 'lng': -122.4227504 }
-    ]
-};
 
 define(function (require) {
     var app = {
