b3ccf8758e6c9d64e47b54505ca115956a7b2171
Amanda Canyon <acanyon@gmail.com>
2015-05-27 11:27:03 -0700

    updates to schema?

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,6 @@ ActiveRecord::Schema.define(version: 20150518222443) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
