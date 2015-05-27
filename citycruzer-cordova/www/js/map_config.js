c5f089a8017d3ef50d646f9dce2c20186b8ab477
Amanda Canyon <acanyon@gmail.com>
2015-05-27 10:38:19 -0700

    moved old phonegap map assets to cordova build

---
+++ citycruzer-cordova/www/js/map_config.js
@@ -0,0 +1,15 @@
+// should later be loaded from server
+//
+var MAP_CONF = {
+    'center': {
+        'lat': 37.7833, 'lng': -122.4167, // san francisco
+        'zoom': 13
+    },
+
+    'markers': [
+        { 'lat': 37.7966021, 'lng': -122.4233821, 'title': 'Big Swingin\' Cycles', 'snippet': '2260 Van Ness Ave\nSan Francisco, CA 94109' },
+
+        { 'lat': 37.7958912, 'lng': -122.4227504, 'title': 'High Trails Cyclery', 'snippet': '1825 Polk St\nSan Francisco, CA 94109' },
+    ]
+};
+
