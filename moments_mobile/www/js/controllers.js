ecd5d7a954da07922d7a72be6d9c8f708fda58d5
Amanda Canyon <acanyon@gmail.com>
2015-08-03 12:05:08 -0700

    wip - cordova?

--- moments_mobile/www/js/controllers.js
+++ moments_mobile/www/js/controllers.js
@@ -5,12 +5,12 @@ angular.module('starter.controllers', [])
     { username: 'leannagrand', description: 'Summer fun as a raft guide. ::emoji::',
         posted_on: '2hrs', view_count: 12, comment_count: 5,
         photos: [
-            { loves: 2, url: '...',
+            { loves: 2, src: 'img/moments/leannagrand_1.jpg',
               comments: [{ username: 'acanyon', comment: 'Best raft guide ever. Hope to visit again!'},
                          { username: 'acanyon', comment: 'Best raft guide ever. Hope to visit again!'}] },
-            { loves: 8, url: '...',
+            { loves: 8, src: 'img/moments/leannagrand_2.jpg',
               comments: [] },
-            { loves: 0, url: '...',
+            { loves: 0, src: 'img/moments/leannagrand_3.jpg',
               comments: [] },
         ]
     }
