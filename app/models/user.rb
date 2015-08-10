f96f6c9f7292e3a4af1cf49ef00d9aed87e7d080
Amanda Canyon <acanyon@gmail.com>
2015-08-09 17:11:13 -0700

    wip User class / api

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable # added by me. not tested. not default
+         :recoverable, :rememberable, :trackable, :validatable
 
-  has_and_belongs_to_many :moments 
+  has_many :moments 
 
 end
