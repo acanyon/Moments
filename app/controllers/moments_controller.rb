c2d712e6496acd3b12c9765be9c5e4d052194cd5
Amanda Canyon <acanyon@gmail.com>
2015-08-11 13:09:15 -0700

    rendering user's moments

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -3,35 +3,35 @@ class MomentsController < ApplicationController
 
     # all moments for given user
     def index
-        json_response = [{ 
-            id: 109,
-            username: 'leannagrand', members: ['gracetherope', 'karlinthehouse'], description: 'Summer fun as a raft guide. ::emoji::',
-            posted_on: '2hrs', view_count: 12, comment_count: 5,
-            photos: [
-                { snaps: 2, src: 'img/moments/leannagrand_1.jpg',
-                  comments: [{ username: 'acanyon', comment: 'Best raft guide ever. Hope to visit again!'},
-                             { username: 'acanyon', comment: 'Best raft guide ever. Hope to visit again!'}] },
-                { snaps: 8, src: 'img/moments/leannagrand_2.jpg',
-                  comments: [] },
-                { snaps: 0, src: 'img/moments/leannagrand_3.jpg',
-                  comments: [] },
-            ]
-        }, {
-            id: 88,
-            username: 'leannagrand', members: ['gracetherope', 'karlinthehouse'], description: 'Summer fun as a raft guide. ::emoji::',
-            posted_on: '2hrs', view_count: 12, comment_count: 5,
-            photos: [
-                { snaps: 2, src: 'img/moments/leannagrand_1.jpg',
-                  comments: [{ username: 'acanyon', comment: 'Best raft guide ever. Hope to visit again!'},
-                             { username: 'acanyon', comment: 'Best raft guide ever. Hope to visit again!'}] },
-                { snaps: 8, src: 'img/moments/leannagrand_2.jpg',
-                  comments: [] },
-                { snaps: 0, src: 'img/moments/leannagrand_3.jpg',
-                  comments: [] },
-            ]
-        }]   
- 
-        render json: json_response
+#        sample_response = [{ 
+#            id: 109,
+#            username: 'leannagrand', members: ['gracetherope', 'karlinthehouse'], description: 'Summer fun as a raft guide. ::emoji::',
+#            posted_on: '2hrs', view_count: 12, comment_count: 5,
+#            photos: [
+#                { snaps: 2, src: 'img/moments/leannagrand_1.jpg',
+#                  comments: [{ username: 'acanyon', comment: 'Best raft guide ever. Hope to visit again!'},
+#                             { username: 'acanyon', comment: 'Best raft guide ever. Hope to visit again!'}] },
+#                { snaps: 8, src: 'img/moments/leannagrand_2.jpg',
+#                  comments: [] },
+#                { snaps: 0, src: 'img/moments/leannagrand_3.jpg',
+#                  comments: [] },
+#            ]
+#        }, {
+#            id: 88,
+#            username: 'leannagrand', members: ['gracetherope', 'karlinthehouse'], description: 'Summer fun as a raft guide. ::emoji::',
+#            posted_on: '2hrs', view_count: 12, comment_count: 5,
+#            photos: [
+#                { snaps: 2, src: 'img/moments/leannagrand_1.jpg',
+#                  comments: [{ username: 'acanyon', comment: 'Best raft guide ever. Hope to visit again!'},
+#                             { username: 'acanyon', comment: 'Best raft guide ever. Hope to visit again!'}] },
+#                { snaps: 8, src: 'img/moments/leannagrand_2.jpg',
+#                  comments: [] },
+#                { snaps: 0, src: 'img/moments/leannagrand_3.jpg',
+#                  comments: [] },
+#            ]
+#        }]   
+
+        render json: current_user.moments.as_json
     end
 
     # POST create new moment
