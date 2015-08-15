a9cb72d305d82a9b7ba97f82ed79b41264b2d036
Amanda Canyon <acanyon@gmail.com>
2015-08-15 14:50:15 -0700

    ScrollView w/ lock, refresh, anchor track/position

--- moments_mobile/www/js/scroll_view.js
+++ moments_mobile/www/js/scroll_view.js
@@ -16,92 +16,108 @@ var ScrollView = Backbone.View.extend({
         this.REFRESH_POS_FN = function (x) { return (-50/Math.pow(500, 2)) * Math.pow(x - 500, 2) + 50; };
     },
 
+    lock: function () {
+        this._is_locked = true;
+    },
+
+    unlock: function () {
+        this._is_locked = false;
+    },
+
     refresh: function () {
+        // note: does not fully prevent two refreshes. race condition still exists
+        // if animating when refresh is called
+        // TODO - add _clearRefresh method
         clearTimeout(this._refresh_timeout);
 
-        var NUM_RETRIES = 4;
+        var REFRESH_INTERVAL = 50; // ms
+        var NUM_RETRIES = 20;
         var _retry_count = NUM_RETRIES;
         var callback = _.bind(function (is_modified) {
             if (is_modified) {
                 _retry_count = NUM_RETRIES;
-            }
-
-            if (_retry_count > -1) {
-                _retry_count -= 1;
-                console.log('* refresh w/ retries: ' + _retry_count);
                 this._refresh(callback);
+            } else if (_retry_count > -1) {
+                _retry_count -= 1;
+                clearTimeout(this._refresh_timeout);
+                this._refresh_timeout = setTimeout(
+                    _.bind(function () { this._refresh(callback); }, this),
+                    REFRESH_INTERVAL);
             }
         }, this);
+
         this._refresh(callback);
+    },
+
+    track_anchor: function ($el) {
+        this.$anchor = $el = $el || this.$anchor;
+        clearTimeout(this._refresh_timeout); // also doesnt fully prevent race
+        if (this.$anchor) {
+            this._anchor_offset = _.extend({}, $el.offset());
+        }
+    },
+
+    clear_anchor: function () {
+        this.$anchor = undefined;
+        this._anchor_offset = undefined;
+    },
+
+    set_anchor_offset: function (anchor_offset) {
+        this._anchor_offset = _.extend(this._anchor_offset || {},  anchor_offset);
+        this.refresh();
     },
 
     _refresh: function (callback) {
         var ANIMATION_DURATION = 50; //ms
-        var REFRESH_INTERVAL = 100; // ms
         if (this.$anchor) {
-            var delayed_callback = _.bind(function (is_modified) {
-                this._refresh_timeout = setTimeout(
-                    function () { callback(is_modified); },
-                    REFRESH_INTERVAL);
-            }, this);
-
+            // TODO - add 'left' for consistancy
             if (this._anchor_offset.top !== this.$anchor.offset().top) {
                 var deltaY = this._anchor_offset.top - this.$anchor.offset().top;
+                if (deltaY > 10) { deltaY = deltaY / 3; }
                 var css_top = this.$scrollable.position().top + deltaY;
-                this.$scrollable.animate(
-                        { top: css_top }, 
-                        ANIMATION_INTERVAL,
-                        'swing',
-                        _.partial(delayed_callback, true));
+                css_top = (css_top < 0 ? css_top : 0);
+                this.$scrollable.animate({ top: css_top }, ANIMATION_DURATION,
+                                         'swing',_.partial(callback, true));
             } else {
-                delayed_callback(false);
+                callback(false);
             }
         }
     },
 
-    track_anchor: function ($el) {
-        this.$anchor = $el;
-        this._anchor_offset = _.extend({}, $el.offset());
-
-        var position = _.extend({}, $el.position());
-        var $cur = $el.offsetParent();
-        while (! $cur.is(this.$scrollable)) {
-            position.top = position.top + $cur.position().top;
-            position.left = position.left + $cur.position().left;
-            $cur = $cur.offsetParent();
-        }
-        this._anchor_position = position;
-    },
-
     _scrollstart: function (event) {
-        if (event.originalEvent.touches.length === 1) {
+        this._is_scrolling = !this._is_locked;
+        if (this._is_scrolling && event.originalEvent.touches.length === 1) {
             this._touches = [ this.copyTouch(event.originalEvent.touches[0]) ];
         }
     },
 
     _scrollmove: function (event) {
-        var cur_touch = this.copyTouch(event.originalEvent.touches[0]);
-        var prev_touch = this._touches[0];
-        this._touches.unshift(cur_touch);
-        
-        var css_top = this.$scrollable.position().top + cur_touch.clientY - prev_touch.clientY;
-        if (css_top < 0) {
-            this.$scrollable.css({ top: css_top });
-        } else {
-            var scrollY = (this.$scrollable.data('scrollY') || 0) + cur_touch.clientY - prev_touch.clientY;
-            this.$scrollable.data({'scrollY': scrollY});
-            this.$scrollable.css({ top: this.REFRESH_POS_FN(scrollY) });
+        if (this._is_scrolling) {
+            var cur_touch = this.copyTouch(event.originalEvent.touches[0]);
+            var prev_touch = this._touches[0];
+            this._touches.unshift(cur_touch);
+            
+            var css_top = this.$scrollable.position().top + cur_touch.clientY - prev_touch.clientY;
+            if (css_top < 0) {
+                this.$scrollable.css({ top: css_top });
+                setTimeout(_.bind(function() { this.track_anchor(); }, this));
+            } else {
+                var scrollY = (this.$scrollable.data('scrollY') || 0) + cur_touch.clientY - prev_touch.clientY;
+                this.$scrollable.data({'scrollY': scrollY});
+                this.$scrollable.css({ top: this.REFRESH_POS_FN(scrollY) });
+            }
         }
     },
 
     _scrollend: function (event) {
-        if (event.originalEvent.touches.length === 0) {
+        if (this._is_scrolling && event.originalEvent.touches.length === 0) {
             this._touches = undefined;
             this.$scrollable.data({'scrollY': ''});
             if (this.$scrollable.position().top > 0) {
                 this.$scrollable.animate({top: 0}, {easing: 'easeOutQuad', duration: 150});
             }
         }
+        this._is_scrolling = undefined;
     },
 
     copyTouch: function (touch) {
@@ -110,7 +126,6 @@ var ScrollView = Backbone.View.extend({
             clientX: touch.clientX
         };
     }
- 
 
 });
 //_.extend(ScrollView, Backbone.Events);
