f69435d0bc90eddba152e751fd710f4087ad1b45
Amanda Canyon <acanyon@gmail.com>
2015-08-15 13:46:11 -0700

    refresh & track anchor wip

--- moments_mobile/www/js/scroll_view.js
+++ moments_mobile/www/js/scroll_view.js
@@ -16,8 +16,61 @@ var ScrollView = Backbone.View.extend({
         this.REFRESH_POS_FN = function (x) { return (-50/Math.pow(500, 2)) * Math.pow(x - 500, 2) + 50; };
     },
 
-    set_anchor: function ($el) {
-        this.$anchor = $el;this.$anchor = $el;
+    refresh: function () {
+        clearTimeout(this._refresh_timeout);
+
+        var NUM_RETRIES = 4;
+        var _retry_count = NUM_RETRIES;
+        var callback = _.bind(function (is_modified) {
+            if (is_modified) {
+                _retry_count = NUM_RETRIES;
+            }
+
+            if (_retry_count > -1) {
+                _retry_count -= 1;
+                console.log('* refresh w/ retries: ' + _retry_count);
+                this._refresh(callback);
+            }
+        }, this);
+        this._refresh(callback);
+    },
+
+    _refresh: function (callback) {
+        var ANIMATION_DURATION = 50; //ms
+        var REFRESH_INTERVAL = 100; // ms
+        if (this.$anchor) {
+            var delayed_callback = _.bind(function (is_modified) {
+                this._refresh_timeout = setTimeout(
+                    function () { callback(is_modified); },
+                    REFRESH_INTERVAL);
+            }, this);
+
+            if (this._anchor_offset.top !== this.$anchor.offset().top) {
+                var deltaY = this._anchor_offset.top - this.$anchor.offset().top;
+                var css_top = this.$scrollable.position().top + deltaY;
+                this.$scrollable.animate(
+                        { top: css_top }, 
+                        ANIMATION_INTERVAL,
+                        'swing',
+                        _.partial(delayed_callback, true));
+            } else {
+                delayed_callback(false);
+            }
+        }
+    },
+
+    track_anchor: function ($el) {
+        this.$anchor = $el;
+        this._anchor_offset = _.extend({}, $el.offset());
+
+        var position = _.extend({}, $el.position());
+        var $cur = $el.offsetParent();
+        while (! $cur.is(this.$scrollable)) {
+            position.top = position.top + $cur.position().top;
+            position.left = position.left + $cur.position().left;
+            $cur = $cur.offsetParent();
+        }
+        this._anchor_position = position;
     },
 
     _scrollstart: function (event) {
